edb6c69e9e01b785a3dd9662f8f878693dc56171
wilsonLi <489642013@qq.com>
2017-02-22 20:09:14 +0800

    提交代码保存

--- nodeWeb/public/web/html/index/index.js
+++ nodeWeb/public/web/html/index/index.js
@@ -1,2 +1,7 @@
 ﻿/// <reference path="../../lib/mui/js/mui.min.js" />
 /// <reference path="../../lib/jquery/jquery.min.js" />
+mui('#search-btn')[0].addEventListener('tap', searchBtnClick);
+
+function searchBtnClick() {
+    location.href = '../searchpage/searchpage.html';
+}
